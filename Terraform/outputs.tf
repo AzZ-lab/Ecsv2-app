62adcd7fa8219013d40170ff9d24ad6b79df0c0e
azad ekinci <azad.e.ekinci@gmail.com>
2026-02-15 14:51:02 +0000

    cd pipeline

---
+++ Terraform/outputs.tf
@@ -0,0 +1,9 @@
+output "container_name" {
+  value = "app"
+  description = "The name of the ECS container"
+}
+
+output "container_port" {
+  value = var.container_port
+  description = "The port the container listens on"
+}
