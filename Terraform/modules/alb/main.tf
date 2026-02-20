50f816817fc5dad0de148efcda156d148388cf3e
azad ekinci <azad.e.ekinci@gmail.com>
2026-02-20 14:06:41 +0000

    change to green and blue deployments match the same health check

--- Terraform/modules/alb/main.tf
+++ Terraform/modules/alb/main.tf
@@ -116,8 +116,8 @@ resource "aws_lb_target_group" "green" {
   vpc_id      = var.vpc_id
 
   health_check {
-    path                = "/"
-    matcher             = "200"
+    path                = var.health_check_path
+    matcher             = "200-399"
     interval            = 30
     timeout             = 5
     healthy_threshold   = 2
