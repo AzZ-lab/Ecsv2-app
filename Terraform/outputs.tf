e28340f3b14a6db3556e9c7f2111e500da5034f8
azad ekinci <azad.e.ekinci@gmail.com>
2026-02-20 12:15:17 +0000

    changes

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -1,9 +1,25 @@
+output "codedeploy_app_name" {
+  value = module.codedeploy.codedeploy_app_name
+}
+
+output "codedeploy_deployment_group_name" {
+  value = module.codedeploy.deployment_group_name
+}
+
+output "ecs_cluster_name" {
+  value = module.ecs.cluster_name
+}
+
+output "ecs_service_name" {
+  value = module.ecs.service_name
+}
+
+
+
 output "container_name" {
-  value       = "app"
-  description = "The name of the ECS container"
+  value = "app"
 }
 
 output "container_port" {
-  value       = var.container_port
-  description = "The port the container listens on"
+  value = var.container_port
 }
